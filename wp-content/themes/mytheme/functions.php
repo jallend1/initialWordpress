b92c354e07970945caac84752359f93a787282b9
jallend1 <jallend1@gmail.com>
2021-10-02 15:00:39 -0700

    Implements sidebar component into theme

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,7 +1,7 @@
 <?php
 
     function jasonsVerySpecial_script_enqueue() {
-        wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/my.css', array(), '1.0.0', 'all');
+        wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/my.css', array(), get_the_time(), 'all');
         wp_enqueue_script('customjs', get_template_directory_uri() . './js/my.js', array(), get_the_time(), true);
     }
 
@@ -12,10 +12,27 @@
     }
 
     add_action('wp_enqueue_scripts', 'jasonsVerySpecial_script_enqueue');
-    add_action('after_setup_theme', 'jasonsVerySpecial_theme_setup'); 
+    add_action('init', 'jasonsVerySpecial_theme_setup'); 
     
     add_theme_support('custom-background');
     add_theme_support('custom-header');
     add_theme_support('post-thumbnails');
+
+        function jasons_widget_setup() {
+            register_sidebar(
+                array(
+                    'name' => 'Sidebar', 
+                    'id' => 'sidebar-1',
+                    'class' => 'custom',
+                    'description' => 'Standard Sidebar',
+                    'before_widget' => '<aside =id="%1$s" class="widget %2$s">',
+                    'after_widget' => '</aside>',
+                    'before_title' => '<h1 class="widget-title">',
+                    'after_title' => '</h1>',
+                    )
+            );
+        }
+
+        add_action('widgets_init', 'jasons_widget_setup');
 
 ?>
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,20 +1,27 @@
 <?php get_header(); ?>
-
-<?php 
-    if(have_posts()) : 
-        while(have_posts()): the_post(); ?>
-            <h3><?php the_title(); ?></h3>
-            <div class="thumbnail-img">
-                <?php the_post_thumbnail(); ?>
-            </div>
-            <h4>
-                Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a');?> in <?php the_category(); ?>
-            </h4>
-            <p> <?php the_content(); ?> </p>    
-            <hr>
-    <?php
-        endwhile;
+<div class="container">
+    <div class="posts">
+    <?php 
+        if(have_posts()) : 
+            while(have_posts()): the_post(); ?>
+                <div class="post">
+                <h3><?php the_title(); ?></h3>
+                <div class="thumbnail-img">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <h4>
+                    Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a');?> in <?php the_category(); ?>
+                </h4>
+                <p> <?php the_content(); ?> </p>    
+                <hr>
+        </div>
+                <?php
+            endwhile;
         endif;
-?>
+        ?>
+
+</div>
+    <?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
